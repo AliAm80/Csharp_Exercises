c6a857e260fad6a202a22cff8181f3a3550d023f
Ali Amoozadeh <aliamoozadeh80@gmail.com>
2023-07-10 01:02:59 +0330

    added method_Ex1 (Welcome & Split Full Name)

---
+++ Method Exercise/Welcome & Split Full Name/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string full_Name ="";
+            getName(full_Name);
+
+        }
+
+        static void getName(string fullName)
+        {
+            Console.WriteLine("Please Enter Your Full Name :");
+            fullName = Console.ReadLine();
+            validate(fullName);
+            var firstName = fullName.Substring(0, fullName.IndexOf(" "));
+            var lastName = fullName.Substring(fullName.IndexOf(" ") + 1);
+            Console.WriteLine("-------------------------------------------------------------");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"\nHi Dear {firstName} :)");
+            Console.WriteLine($"\nYour first name is {firstName.ToUpper()} and your last name is {lastName.ToUpper()}");
+            Console.ResetColor();
+            
+
+        }
+        static void validate(string fullName)
+        {
+            while (string.IsNullOrEmpty(fullName))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\t\t\t\tError : You Entered Nothing , Please Try Again !!!");
+                Console.ResetColor();
+                getName(fullName);
+
+            }
+
+        }
+
+    }
+}
