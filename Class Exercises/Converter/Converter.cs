033ea035147a4ea1257ac94eb61246654f74a47f
Ali Amoozadeh <aliamoozadeh80@gmail.com>
2023-07-26 11:50:20 +0330

    optimized & updated class_Ex4
    1) added new feature (alphabet & day of week types)
    2) optimized classes

--- Class Exercises/Converter/Converter.cs
+++ Class Exercises/Converter/Converter.cs
@@ -7,13 +7,92 @@ namespace Ex_Converter
 {
     public static class Converter
     {
-        public static double MilesToKilometers(double mile)
+        public static void MilesToKilometers(double mile)
         {
-            return mile * 1.6;
+            var result = mile * 1.6;
+            Console.WriteLine($"\n{mile} miles is equal to {result} kilometers\n");
         }
-        public static double KilometersToMiles(double kilometer)
+        public static void KilometersToMiles(double kilometer)
         {
-            return kilometer / 1.6;
+            var result = kilometer / 1.6;
+            Console.WriteLine($"\n{kilometer} kilometers is equal to {result} miles\n");
+        }
+        public static void NumberToAlphabet(int number)
+        {
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine(" Result --> One ");
+                    break;
+                case 2:
+                    Console.WriteLine(" Result --> Two ");
+                    break;
+                case 3:
+                    Console.WriteLine(" Result --> Three ");
+                    break;
+                case 4:
+                    Console.WriteLine(" Result --> Four ");
+                    break;
+                case 5:
+                    Console.WriteLine(" Result --> Five ");
+                    break;
+                case 6:
+                    Console.WriteLine(" Result --> Six ");
+                    break;
+                case 7:
+                    Console.WriteLine(" Result --> Seven ");
+                    break;
+                case 8:
+                    Console.WriteLine(" Result --> Eight ");
+                    break;
+                case 9:
+                    Console.WriteLine(" Result --> Nine ");
+                    break;
+                case 10:
+                    Console.WriteLine(" Result --> Ten ");
+                    break;
+
+                default:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("ERROR : You entered out of range !!!");
+                    Console.ResetColor();
+                    break;
+
+            }
+        }
+
+        public static void NumberToDayOfWeek(int number)
+        {
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("Today is Saturday :)");
+                    break;
+                case 2:
+                    Console.WriteLine("Today is Sunday :)");
+                    break;
+                case 3:
+                    Console.WriteLine("Today is Monday :)");
+                    break;
+                case 4:
+                    Console.WriteLine("Today is Tuesday :)");
+                    break;
+                case 5:
+                    Console.WriteLine("Today is Wednesday :)");
+                    break;
+                case 6:
+                    Console.WriteLine("Today is Thursday :)");
+                    break;
+                case 7:
+                    Console.WriteLine("Today is Friday :)");
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("ERROR : You entered out of range !!!");
+                    Console.ResetColor();
+                    break;
+            }
+
         }
     }
 }
--- Class Exercises/Converter/ShowInput.cs
+++ Class Exercises/Converter/ShowInput.cs
@@ -7,18 +7,77 @@ namespace Ex_Converter
 {
     public static class ShowInput
     {
-        public static double MilesShow()
+        public static int Menu()
+        {
+            Console.WriteLine("1) Convert Miles To Kilometers");
+            Console.WriteLine("2) Convert Kilometers To Miles");
+            Console.WriteLine("3) Convert Number To Alphabet");
+            Console.WriteLine("4) Convert Number To Day Of Week");
+            Console.WriteLine("5) Exit");
+            Console.Write("\nEnter a number to execute : ");
+            var select = Convert.ToInt32(Console.ReadLine());
+            return select;
+        }
+
+        public static void Choose(int select)
+        {
+            switch (select)
+            {
+                case 1:
+                    var input = MilesShow();
+                    Converter.MilesToKilometers(input);
+                    break;
+                case 2:
+                    input = KilometersShow();
+                    Converter.KilometersToMiles(input);
+                    break;
+                case 3:
+                    input = AlphabetShow();
+                    Converter.NumberToAlphabet((int)input);
+                    break;
+                case 4:
+                    input = DayOfWeekShow();
+                    Converter.NumberToDayOfWeek((int)input);
+                    break;
+                case 5:
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine("Good Bye :)\n");
+                    Console.ResetColor();
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("ERROR : You entered wrong item !!!");
+                    Console.ResetColor();
+                    Console.WriteLine("Try again my friend :)");
+                    break;
+
+            }
+        }
+        static double MilesShow()
         {
             Console.Write("Enter the distance in miles : ");
             var number = Convert.ToDouble(Console.ReadLine());
             return number;
         }
-        public static double KilometersShow()
+        static double KilometersShow()
         {
             Console.Write("Enter the distance in kilometers : ");
             var number = Convert.ToDouble(Console.ReadLine());
             return number;
         }
-        
+        static int AlphabetShow()
+        {
+            Console.Write("Enter a number between 1 to 10 : ");
+            var number = Convert.ToInt32(Console.ReadLine());
+            return number;
+        }
+        static int DayOfWeekShow()
+        {
+            Console.Write("Enter a number between 1 to 7 for day of the week : ");
+            var number = Convert.ToInt32(Console.ReadLine());
+            return number;
+        }
+
     }
 }
