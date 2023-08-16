5ee66a4c690e81286e0d63c3b063b99d553e3d82
Ali Amoozadeh <aliamoozadeh80@gmail.com>
2023-08-16 19:46:47 +0330

    Added live coding Ch_2 (Reverse And Not)

---
+++ Live Coding/Reverse And Not/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Challange_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter a Number : ");
+            var number = Console.ReadLine();
+            Console.WriteLine(ReverseAndNot(number));
+        }
+
+        static string ReverseAndNot(string number)
+        {
+            var temp = "";
+            for (int i = number.Length - 1; i >= 0; i--)
+            {
+                temp += number[i];
+            }
+
+            return temp + number;
+
+        }
+    }
+}
+
